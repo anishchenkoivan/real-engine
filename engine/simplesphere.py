685fba3f3162b99e3738283c8f8d46df4b8ed5e5
Ivan Anishchenko <ivananisenko08@gmail.com>
2025-02-06 21:44:29 +0300

    Restructure scene setup

--- engine/simplesphere.py
+++ engine/simplesphere.py
@@ -1,6 +1,30 @@
 from graphics import *
 from OpenGL.GL import *
 import config
+import engine
+
+
+def main():
+    engine_instance = engine.Engine()
+    vertex_shader = Shader("../shaders/vertex-shader.vert")
+    fragment_shader = Shader("../shaders/fragment-shader.frag")
+    shader = ShaderProgram(vertex_shader, fragment_shader)
+    logic_provider = SimpleSphereLogicProvider(shader)
+
+    # Cube vertices (positions + colors)
+    vertices = np.array([
+        -1.0, -1.0,
+        1.0, -1.0,
+        1.0, 1.0,
+
+        1.0, 1.0,
+        -1.0, 1.0,
+        -1.0, -1.0,
+    ], dtype=np.float32)
+
+    mesh = VerticesMesh(vertices)
+    renderer = Renderer(shader, MeshGroup(mesh), logic_provider)
+    engine_instance.run(renderer)
 
 
 class SimpleSphereLogicProvider(LogicProvider):
